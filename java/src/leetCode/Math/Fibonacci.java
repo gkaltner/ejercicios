9caab466e608a70eaf87d832d28b2ff6878478b7
gaston <saltador197>
2018-07-16 10:45:23 -0300

    otra version del fibonacci

--- java/src/leetCode/Math/Fibonacci.java
+++ java/src/leetCode/Math/Fibonacci.java
@@ -5,10 +5,12 @@ package leetCode.Math;
  */
 public class Fibonacci {
     public static void main(String []args){
-        fibonacci(150);
+        for (int i = 0; i < 20; i++) {
+            System.out.print(fibonacci(i)+" ");
+        }
     }
 
-    public static void fibonacci(int limit){
+    /*public static void fibonacci(int limit){
         fibonacci(limit,1,0);
     }
 
@@ -19,5 +21,17 @@ public class Fibonacci {
         System.out.println(curent);
         int next = curent + previous;
         fibonacci(limit,next,curent);
+    }*/
+
+    public static int fibonacci(int number) {
+        if(number > 1) {
+          return fibonacci(number-1) + fibonacci(number -2);
+        }
+        if(number == 1 || number == 0){
+            return number;
+        } else {
+            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            return -1;
+        }
     }
 }
